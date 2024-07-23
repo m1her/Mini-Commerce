c6b7468a2197cd7e722c7644b5ed20b3d1e33f82
MaherNassrallah <106315157+m1her@users.noreply.github.com>
2024-07-23 21:31:26 +0300

    Add files via upload

---
+++ src/app/cart/loading.tsx
@@ -0,0 +1,14 @@
+// components/Loading.js
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="md:py-4 md:px-8 py-2 px-4 w-full flex justify-center items-center min-h-screen relative bg-gradient-to-tl from-cyan-400/90 via-violet-400/90 to-rose-200/90">
+      <div className="w-full h-[80vh] bg-white/70 lg:grid flex flex-col grid-cols-9 rounded mt-10">
+
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
